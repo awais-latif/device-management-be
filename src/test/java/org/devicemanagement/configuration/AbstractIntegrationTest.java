cc5c055f3d703b614555cec24f87a703ee2cf3c8
Awais Latif <34969521+awais-latif@users.noreply.github.com>
2026-09-19 15:17:30 +0200

    Add CI github action support  - removed default profile  - add db config for tests

---
+++ src/test/java/org/devicemanagement/configuration/AbstractIntegrationTest.java
@@ -0,0 +1,23 @@
+package org.devicemanagement.configuration;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class AbstractIntegrationTest {
+
+    static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16-alpine")
+                    .withReuse(true);
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
