ec3300d303d8cd1ed727a86914d295b2ffdfca45
Awais Latif <k.awaislatif@gmail.com>
2026-09-19 20:52:31 +0200

    change project namespace

--- src/test/java/com/example/devicemanagement/configuration/AbstractIntegrationTest.java
+++ src/test/java/com/example/devicemanagement/configuration/AbstractIntegrationTest.java
@@ -1,4 +1,4 @@
-package org.devicemanagement.configuration;
+package com.example.devicemanagement.configuration;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
